bd2a199798f800df23820d046e79acbc775e8f61
Prrr <pabloriera>
2014-10-01 21:18:29 -0300

    actualizado

--- ofxControlFloat_example/obj/x86_64Debug/src/ofxControlFloat.d
+++
@@ -1,4 +0,0 @@
-obj/x86_64Debug/src/ofxControlFloat.o: src/ofxControlFloat.cpp \
- src/ofxControlFloatClass.h
-
-src/ofxControlFloatClass.h:
--- ofxControlFloat_example/obj/x86_64Release/addons/ofxControlFloat/src/ofxControlFloat.d
+++
@@ -1,5 +0,0 @@
-obj/x86_64Release/addons/ofxControlFloat/src/ofxControlFloat.o: \
- ../../../addons/ofxControlFloat/src/ofxControlFloat.cpp \
- ../../../addons/ofxControlFloat/src/ofxControlFloat.h
-
-../../../addons/ofxControlFloat/src/ofxControlFloat.h:
